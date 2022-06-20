8eb4578d3aea3fbace48f94c52da1b0c7ce0be17
Shankar <shankars0899@gmail.com>
2022-06-20 10:56:51 +0530

    created request validator

---
+++ scripts/L2/listenRequestValidator.ts
@@ -0,0 +1,48 @@
+/* eslint-disable node/no-missing-import */
+import { getContract } from "../helper/helperContractAddress";
+
+import { GenerateKey } from "./generateKey";
+import { eventListenerKeyExposed } from "./listenKeyExposed";
+
+// eslint-disable-next-line node/no-extraneous-require
+const Web3 = require("web3");
+
+const API_URL = process.env.STORKNET_URL;
+const web3 = new Web3(API_URL);
+
+const CONTRACT_ADDRESS: string = getContract("StorkRequestHandler");
+
+const contract = require("../../artifacts/contracts/StorkRequestHandler.sol/StorkRequestHandler.json");
+const StorkRequestHandler = new web3.eth.Contract(contract.abi, CONTRACT_ADDRESS);
+
+let _fromBlock: number = 0;
+const listenEvent: string = "RequestValidator";
+
+function eventListener() {
+  StorkRequestHandler.getPastEvents(
+    listenEvent,
+    {
+      fromBlock: "latest",
+    },
+    function (_error: any, events: any) {
+      if (events[0] !== undefined) {
+        if (events[0].blockNumber >= _fromBlock) {
+          for (let i = 0; i < events.length; i++) {
+            console.log(
+              `\n[+]New Block with transaction ${events[i].transactionHash} at block number ${events[i].blockNumber}\n${events[i].returnValues._reqId} - ${events[i].returnValues.miner} - ${events[i].returnValues._fallbackFunction} \n${events[i].returnValues.zkChallenge} \n${events[i].returnValues.data}`
+            );
+            if (events[i].returnValues.miner == process.env.PUBLIC_KEY_OWNER) {
+              GenerateKey(events[i].returnValues._reqId);
+              eventListenerKeyExposed();
+            }
+          }
+          _fromBlock = events[events.length - 1].blockNumber + 1;
+        }
+      }
+    }
+  );
+  setTimeout(eventListener, 10 * 1000);
+}
+
+console.log("start listening for " + listenEvent);
+eventListener();
